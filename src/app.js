a82b415fc18ff0468d1e04d233b2e6950d669532
heyjk2212 <jinakim2212@gmail.com>
2023-11-25 13:00:27 +0900

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import UsersRouter from "./routes/users.router.js";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json()); // body-parser
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", [UsersRouter]);
+
+app.listen(PORT, () => {
+  console.log(PORT, `Server running on port ${PORT}`);
+});
---
+++ src/routes/users.router.js
@@ -0,0 +1,7 @@
+import express from "express";
+import authMiddleware from "../middlewares/auth.middleware.js";
+import {} from "../validation/joi.js";
+
+const router = express.Router();
+
+export default router;
