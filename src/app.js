cace05e59733fe2b9ab88f47f0771ae014969dd0
heyjk2212 <jinakim2212@gmail.com>
2023-11-26 14:48:18 +0900

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,33 @@
 import express from "express";
 import UsersRouter from "./routes/users.router.js";
+import ProfileRouter from "./routes/profile.router.js";
+import SongsRouter from "./routes/songs.router.js";
+import DiariesRouter from "./routes/diaries.router.js";
+import GuestbookRouter from "./routes/guestbook.router.js";
+import PostsRouter from "./routes/posts.router.js";
+import CommentsRouter from "./routes/comments.router.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json()); // body-parser
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.use("/api", [UsersRouter]);
+app.get("/", (req, res) => {
+  res.json({ message: "success" });
+});
+
+app.use("/api", [
+  UsersRouter,
+  ProfileRouter,
+  SongsRouter,
+  DiariesRouter,
+  GuestbookRouter,
+  PostsRouter,
+  CommentsRouter,
+]);
 
 app.listen(PORT, () => {
   console.log(PORT, `Server running on port ${PORT}`);
