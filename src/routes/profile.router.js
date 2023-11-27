e859e15fea9c916b8a5c8244d101836a6f8a5510
heyjk2212 <jinakim2212@gmail.com>
2023-11-27 22:20:28 +0900

    Edit image upload part

--- src/routes/profile.router.js
+++ src/routes/profile.router.js
@@ -2,34 +2,36 @@ import express from "express";
 import { prisma } from "../utils/prisma/index.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
 import { profileSchema } from "../validation/joi.js";
-// import AWS from "aws-sdk";
-// import multer from "multer";
-// import multerS3 from "multer-s3";
-// import path from "path";
+import multer from "multer";
+import fs from "fs";
+import multerS3 from "multer-s3";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+
 import dotenv from "dotenv";
 dotenv.config();
 
-// AWS 설정 로드
-// AWS.config.update({
-//   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-// });
+// AWS SDK v3 S3 클라이언트 생성
+const s3Client = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
 
 const router = express.Router();
 
-// let s3 = new AWS.S3();
-
-// const upload = multer({
-//   storage: multerS3({
-//     s3: s3,
-//     bucket: "hanghae99-assets",
-//     key: function (req, file, cb) {
-//       let extension = path.extname(file.originalname);
-//       cb(null, Date.now().toString() + extension);
-//     },
-//     acl: "public-read-write",
-//   }),
-// });
+// Multer-S3 설정에 대한 코드 수정
+const upload = multer({
+  storage: multerS3({
+    s3: s3Client,
+    bucket: "hanghae99-assets",
+    acl: "public-read",
+    key: (req, file, cb) => {
+      cb(null, `${Date.now().toString()}-${file.originalname}`);
+    },
+  }),
+});
 
 // 프로필 등록
 router.post(
@@ -84,51 +86,56 @@ router.post(
 );
 
 // 프로필 조회
-router.get("/users/:memberId/profile", async (req, res, next) => {
-  try {
-    // const { memberId } = req.params;
-    const validateParams = await profileSchema.validateAsync(req.params);
-    const { memberId } = validateParams;
+router.get(
+  "/users/:memberId/profile",
+  upload.single("image"),
+  async (req, res, next) => {
+    try {
+      // const { memberId } = req.params;
+      const validateParams = await profileSchema.validateAsync(req.params);
+      const { memberId } = validateParams;
 
-    const user = await prisma.users.findFirst({
-      where: {
-        memberId: +memberId,
-      },
-    });
+      const user = await prisma.users.findFirst({
+        where: {
+          memberId: +memberId,
+        },
+      });
 
-    if (!user) {
-      return res
-        .status(400)
-        .json({ errorMessage: "유저가 존재하지 않습니다." });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errorMessage: "유저가 존재하지 않습니다." });
+      }
+
+      const userProfile = await prisma.profile.findFirst({
+        where: {
+          MemberId: +memberId,
+        },
+        select: {
+          MemberId: true,
+          mood: true,
+          profileImage: true,
+          introduction: true,
+          message: true,
+          updatedAt: true,
+        },
+      });
+
+      return res.status(200).json({ data: userProfile });
+    } catch (error) {
+      console.error(error);
+
+      return res.status(500).json({ error: "서버 에러" });
     }
-
-    const userProfile = await prisma.profile.findFirst({
-      where: {
-        MemberId: +memberId,
-      },
-      select: {
-        MemberId: true,
-        mood: true,
-        profileImage: true,
-        introduction: true,
-        message: true,
-        updatedAt: true,
-      },
-    });
-
-    return res.status(200).json({ data: userProfile });
-  } catch (error) {
-    console.error(error);
-
-    return res.status(500).json({ error: "서버 에러" });
-  }
-});
+  },
+);
 
 // 프로필 수정
+// 이미지 업로드 엔드포인트
 router.patch(
   "/users/:memberId/profile",
   authMiddleware,
-  // upload.single("file"),
+  upload.single("file"),
   async (req, res, next) => {
     try {
       // const { memberId } = req.params;
@@ -138,6 +145,9 @@ router.patch(
       const { memberId } = validateParams;
       const { mood, message, introduction } = validation;
       const user = req.user;
+
+      // 업로드된 이미지의 URL 반환
+      const imageUrl = req.file.location; // S3에 업로드된 이미지의 URL
 
       // 프로필 주인이 로그인한 사용자와 일치하는지 확인
       if (user.memberId !== +memberId) {
@@ -149,18 +159,6 @@ router.patch(
       // 파일 정보 확인을 위한 콘솔 로그
       console.log(req.file);
 
-      // 업로드된 파일은 req.file 객체에 저장
-      // const file = req.file; // 업로드된 파일 정보
-
-      // const params = {
-      //   Bucket: "hanghae99-assets",
-      //   Key: file.originalname,
-      //   Body: file.buffer, // 파일의 데이터
-      // };
-
-      // S3에 파일 업로드
-      //const result = await s3.upload(params).promise();
-
       // S3에 파일 업로드된 URL을 프로필 업데이트에 사용
       await prisma.profile.update({
         where: {
@@ -169,7 +167,7 @@ router.patch(
         data: {
           mood,
           message,
-          // profileImage: result.Location, // S3에 업로드된 파일의 URL
+          profileImage: imageUrl, // S3에 업로드된 파일의 URL
           introduction,
         },
       });
