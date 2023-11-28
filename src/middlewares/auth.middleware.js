6de884cf4fbc7c0f13b49d84d457d0d56ae8c2b7
heyjk2212 <jinakim2212@gmail.com>
2023-11-28 10:21:56 +0900

    Delete unnecessary files

--- src/middlewares/auth.middleware.js
+++
@@ -1,4 +0,0 @@
-import jwt from "jsonwebtoken";
-import { prisma } from "../utils/prisma/index.js";
-
-export default async function (req, res, next) {}
--- src/routes/users.router.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import authMiddleware from "../middlewares/auth.middleware.js";
-import {} from "../validation/joi.js";
-
-const router = express.Router();
-
-export default router;
